c3ac5186f3037eb3ba0b81e2e9c2b31e3281c6d1
AKASH KUAMR <akashkumar0824@gmail.com>
2017-09-30 01:13:58 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mytry',
+      version='0.1',
+      description='First try for package',
+      url='http://github.com/storborg/funniest',
+      author='Akash',
+      author_email='akashkumar0824@gmail.com',
+      license='MIT',
+      packages=['mytry'],
+      zip_safe=False)
